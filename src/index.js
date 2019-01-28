df2b0d3a696b0a1cbbe035a46f3ed8d1c5f0c3e2
Florencio Varela <florenciovarela@avalith.net>
2019-01-28 17:55:06 -0300

    adding redux middleware

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'; 
 import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger'
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
-import App from './containers/App'
+import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { searchRobots } from './reducers';
 
  const logger = createLogger();
- const store = createStore(searchRobots, applyMiddleware(logger));
+ const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 ReactDOM.render( <Provider store={store}>
                     <App />
